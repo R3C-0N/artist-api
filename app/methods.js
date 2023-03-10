97a2cfae172f6b487a7520f28b23a300f9ee685c
Medard Mathis <mathis.medard@infomaniak.com>
2023-03-10 14:33:04 +0100

    feat(methods): add artist search method in list

--- app/methods.js
+++ app/methods.js
@@ -1,9 +1,16 @@
+import ArtistController from "./controllers/artist.controller.js";
+
 const methods = {
     // Here are registered the different methods
-    "GET" :{},
-    "POST" :{},
-    "PUT" :{},
-    "DELETE" :{}
+    "GET" :{
+        "artist.search": [ArtistController, "artistSearch"],
+    },
+    "POST" :{
+    },
+    "PUT" :{
+    },
+    "DELETE" :{
+    }
 };
 
 export default methods;
